2587c3b202c7f7cdcc288f58d3d0e8ddff005731
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-10-16 12:52:35 +0000

    [Add]新規登録/ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  
+  # 管理者用
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
 
 
+  # 顧客用
   devise_for :customers,skip: [:passwords],controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
