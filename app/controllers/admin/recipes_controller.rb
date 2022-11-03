0fb7e23867a0a70647fcf832331343fd6686e2ef
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-11-03 14:15:53 +0000

    [Add]管理側レシピ一覧ページ

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -6,5 +6,40 @@ class Admin::RecipesController < ApplicationController
     @recipe = Recipe.new
   end
   
+  def index
+    @recipes = Recipe.page(params[:page]).per(10)
+  end
+  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to  admin_recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+    redirect_to admin_recipe_path(@recipe)
+    else
+    render:edit
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:image, :title, :serving, :store_id, :genre_id, :comment,:status)
+  end
   
 end
