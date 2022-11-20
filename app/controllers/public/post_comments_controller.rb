4c9fdd976e4991b73179a1650141a7497e22f118
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-11-20 14:42:56 +0000

    [Update]レビュー機能

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,11 +1,11 @@
 class Public::PostCommentsController < ApplicationController
   
   def create
-    @post_image = PostImage.find(params[:post_image_id])
-    @comment = current_cosutomer.post_comments.new(post_comment_params)
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = current_customer.post_comments.new(post_comment_params)
     @comment.recipe_id = @recipe.id
     @comment.save
-    redirect_to recipe_path(recipe)
+    redirect_to recipe_path(@recipe)
   end
   
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_20_074801) do
+ActiveRecord::Schema.define(version: 2022_11_20_143453) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -125,7 +125,6 @@ ActiveRecord::Schema.define(version: 2022_11_20_074801) do
   create_table "post_comments", force: :cascade do |t|
     t.integer "recipe_id", null: false
     t.integer "customer_id", null: false
-    t.string "title", null: false
     t.text "comment", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
