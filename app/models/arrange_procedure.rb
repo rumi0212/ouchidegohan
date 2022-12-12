7c6eeec5d588b78928c282b5c2d61160128c9ecb
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-12-12 13:24:56 +0000

    [Update]バリデーション

--- app/models/arrange_procedure.rb
+++ app/models/arrange_procedure.rb
@@ -1,4 +1,9 @@
 class ArrangeProcedure < ApplicationRecord
-  
+
   belongs_to :arrange_recipe
+
+  with_options presence: true do
+    validates :body
+  end
+
 end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,3 +1,8 @@
 class Procedure < ApplicationRecord
   belongs_to :recipe
+
+  with_options presence: true do
+    validates :body
+  end
+
 end
